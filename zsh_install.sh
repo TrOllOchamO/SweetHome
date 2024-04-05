b3f1b9ebb3a742d4cc5c605dd47807baccdbe7ca
TrOllOchamO <83348492+TrOllOchamO@users.noreply.github.com>
2024-04-05 14:35:23 +0200

    Update zsh_install.sh

--- zsh_install.sh
+++ zsh_install.sh
@@ -6,7 +6,6 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 
 # install theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
