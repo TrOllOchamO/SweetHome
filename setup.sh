7d73275c4abefcd09dfea8d2cdec011545887f2d
TrOllOchamO <83348492+TrOllOchamO@users.noreply.github.com>
2024-04-05 10:01:40 +0200

    add chmod

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/dash
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
 sudo apt install wget -y
 sudo apt install tree -y
 sudo apt install fortune -y
 sudo apt install cowsay -y
+
+chmod +x *.sh
 
 ./tmux_install.sh
 ./helix_install.sh
