7368c8f99e1c9aa2ff7d35244e4b18be337968f0
TrOllOchamO <barnabe.perrault@gmail.com>
2024-04-23 22:43:30 +0200

    modif theme

--- zsh_install.sh
+++ zsh_install.sh
@@ -5,7 +5,7 @@ chsh -s $(which zsh)  # set zsh as default shell
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" 
 
 # install theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cp .zshrc ~/.zshrc
 cp .p10k.zsh ~/.p10k.zsh
